85f1eb3fe939f3e134f4e3b365b628ed5c3624df
am094137 <aniket.mehta@cerner.com>
2023-10-10 19:27:24 +0530

    adding the routes configs

---
+++ app/controllers/api/v1/numapi_controller.rb
@@ -0,0 +1,18 @@
+require 'net/http'
+module Api
+    module V1
+        class NumapiController < ApplicationController
+            def fetch_api_result
+                #user input
+                user_input = params[:user_input]
+                #custom endpoint
+                ext_url = "http://numbersapi.com/#{user_input}"
+                uri = URI(ext_url)
+                response = Net::HTTP.get(uri)
+                #Response
+                render plain: response
+            end
+        end
+    end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-
-  resources :numapi, only: [] do
-    collection do
-      get ':user_input', action: :fetch_api_result
+  namespace :api do
+    namespace :v1 do
+      resources :numapi, only: [] do
+        collection do
+          get ':user_input', action: :fetch_api_result
+        end
+      end
     end
   end
 end
